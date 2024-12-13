996b672016039ebc4d289912ed20af3ae549ae00
kaijuxkitty <kaijuxkitty@gmail.com>
2024-12-13 13:49:56 -0600

    initial commit

---
+++ prisma/seed.js
@@ -0,0 +1,18 @@
+const prisma = require("../prisma");
+const { faker } = require("@faker-js/faker");
+
+const seed = async (numTracks = 20) => {
+  // Create tracks
+  const tracks = Array.from({ length: numTracks }, () => ({
+    name: faker.music.songName(),
+  }));
+  await prisma.track.createMany({ data: tracks });
+};
+
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
